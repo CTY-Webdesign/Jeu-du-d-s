827ebed1523a25a4f467e41c8f05bc6a51a1ff0d
Thomas Coutaye <coutaye.thomas97@gmail.com>
2024-02-14 19:05:31 +0400

    ajout player1

---
+++ script.js
@@ -0,0 +1,15 @@
+let roundscore=0;
+let score_P1=10;
+let score_P2=0;
+
+
+function scoreP1() {
+        
+    document.getElementById('P1').innerText =score_P1;
+
+}
+
+function updateScore_H() {
+    // Met à jour le contenu de l'élément avec l'ID "scoreValue_H"
+    document.getElementById('P1').innerText = score_P1;
+}
